64604011795705db2e5d930b338758b16ea3a83f
Caio <caiopadilha@id.uff.br>
2022-10-02 17:34:06 -0300

    test: login sem sucesso, dados incorretos

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -20,17 +20,43 @@ describe('Testes de login', () => {
     const loginMock = {
       email: 'admin@admin.com.br',
       password: '12345678'
-    }
+    };
 
     before(async () => {
-      response = await (chai.request(app).post('/login').send(loginMock as userModel))
+      response = await (chai.request(app).
+      post('/login').
+      send(loginMock as userModel))
     });
+
     it('status 200', () => {
       expect(response).to.have.status(200);
-    })
+    });
+
     it('Retorna o token do usuário', () => {
       expect(response.body).to.have.property('token');
-    })
-  }
-  )
+    });
+  });
+
+  describe('login incorreto', () => {
+    let response: Response;
+
+    const loginMock = {
+      email: 'caio@caio.com.br',
+      password: '111111111'
+    };
+
+    before(async () => {
+      response = await (chai.request(app).
+      post('/login').
+      send(loginMock as userModel))
+    });
+
+    it('status 401', () => {
+      expect(response).to.have.status(401);
+    });
+
+    it('Retorna mensagem de email ou senha incorretos', () => {
+      expect(response.body.message).to.be.equal('Incorrect email or password');
+    });
+  });
 });
